969f76c84e5a9d261edca453b035881c3c2d05f5
lisalidroit <54855689+lisalidroit@users.noreply.github.com>
2019-12-18 09:40:48 -0500

    Update replaceLinks.sh

--- replaceLinks.sh
+++ replaceLinks.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+# A Simple Shell Script That Updates Old Annotation Links With the New Ones
+
+# if affectedFiles.txt does not exist then create one, otherwise clear it to be an empty txt file
 if [[ ! -e affectedFiles.txt ]]; then
     touch affectedFiles.txt
 else
     cat /dev/null > affectedFiles.txt
 fi
 
+
 n=1
+
+# Reads each line in the mapping file, separated by ,
 while IFS=, read -r a b;
 do
+# skips first line
 if [ $n -ne 1 ]
 then  
-
+        # find all files with old annotation links
         files=$(grep -rl "$a" $2)
+        
+        # for each file, write the file name to the affectedFiles.txt and replace the old link
         for item in $files
         do
             echo "Affected files with old link: $a , new link: $b" >> affectedFiles.txt
@@ -19,6 +28,12 @@ then
             sed -i '' "s#${a}#${b}#g" $item
         done
 fi
+
+# increment n
 n=$((n+1))
 done < "$1"
+
+# prints done upon finish
 echo "done"
+
+
